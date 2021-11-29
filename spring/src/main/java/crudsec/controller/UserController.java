a8253a24587151a33c0891ab8adb92a536653f5f
HosmanP <hosman@mail.ru>
2021-11-29 17:00:09 +0300

    сделано

---
+++ spring/src/main/java/crudsec/controller/UserController.java
@@ -0,0 +1,28 @@
+package crudsec.controller;
+
+import crudsec.model.User;
+import crudsec.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/user")
+    public String getUsers(ModelMap model, @AuthenticationPrincipal UserDetails currentUser) {
+        User user = userService.findByUserName(currentUser.getUsername());
+        model.addAttribute("user", user);
+        return "user";
+    }
+}
