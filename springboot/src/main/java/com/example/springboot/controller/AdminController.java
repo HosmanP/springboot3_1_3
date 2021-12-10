39e58f02c50d52178bb111f78d4083c5747c2656
HosmanP <hosman@mail.ru>
2021-12-10 21:03:39 +0300

    готово 3.1.2

--- springboot/src/main/java/com/example/springboot/controller/AdminController.java
+++ springboot/src/main/java/com/example/springboot/controller/AdminController.java
@@ -54,16 +54,18 @@ public class AdminController {
         return "redirect:/admin/";
     }
 
-    @GetMapping("updateInfo/{id}")
-    public String updateInfo(@PathVariable("id") Long id, ModelMap model) {
-        User user = userService.getUserById(id);
-        Set<Role> listRoles = roleService.getAllRoles();
-        model.addAttribute("user", user);
-        model.addAttribute("allRoles", listRoles);
-        return "user-info";
+    @PatchMapping("edit/{id}")
+    public String edit(@ModelAttribute("user") User user, @RequestParam("roles") String[] roles) {
+        Set<Role> roleSet = new HashSet<>();
+        for (String role : roles) {
+            roleSet.add(roleService.findByRoleName(role));
+        }
+        user.setRoles(roleSet);
+        userService.saveUser(user);
+        return "redirect:/admin/";
     }
 
-    @GetMapping("deleteUser/{id}")
+    @DeleteMapping("delete/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
         userService.removeUser(id);
         return "redirect:/admin/";
@@ -79,6 +81,4 @@ public class AdminController {
         userService.updateUser(user,id);
         return "redirect:/admin/";
     }
-
-
 }
